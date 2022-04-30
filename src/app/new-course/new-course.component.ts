a9e3272510da1f0f79bedece6036d67dd8cdb116
pittner <lubos.pittner@posam.sk>
2022-04-30 11:39:32 +0200

    new course

--- src/app/new-course/new-course.component.ts
+++ src/app/new-course/new-course.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {Course} from "../course";
+import {CoursesService} from "../courses.service";
+import {Router} from "@angular/router";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-new-course',
-  templateUrl: './new-course.component.html',
-  styles: [
-  ]
+  templateUrl: './new-course.component.html'
 })
-export class NewCourseComponent implements OnInit {
+export class NewCourseComponent {
 
-  constructor() { }
+  item = {} as Course;
 
-  ngOnInit(): void {
+  constructor(private courses: CoursesService,
+              private router: Router) { }
+
+
+  submit(form: NgForm) {
+    if(form.invalid){
+      return
+    }
+
+    this.courses.add(this.item)
+      .subscribe((id) => {
+        this.router.navigate(['/courses', id])
+      })
   }
-
 }
