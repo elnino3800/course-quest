937762064724180d333d72a521627ba9a943464b
pittner <lubos.pittner@posam.sk>
2022-04-30 09:24:15 +0200

    name count saving

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LocalStorageService} from "angular-2-local-storage";
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  name = 'Lubos';
-  count = 0;
+  name: string;
+  count: number;
+
+  constructor(private localStorageService: LocalStorageService) {
+    this.name = localStorageService.get('name') || 'Lubos';
+    this.count = localStorageService.get('count') || 0;
+  }
 
   increment() {
     this.count++;
+    this.localStorageService.set('count', this.count);
   }
 
+  onNameChange() {
+    this.localStorageService.set('name', this.name);
+  }
 }
